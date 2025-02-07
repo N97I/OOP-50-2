3fa1af6b63207cc458e2bdd6503f44684a8f79a7
N97I <N97I-ildan3216@gmail.com>
2025-02-07 21:27:28 +0600

    HW изhw2

---
+++ hw/hw2.py
@@ -0,0 +1,50 @@
+class Heroes:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+
+    def action(self):
+        print(f"{self.name} готов к бою!")
+
+    def attack(self):
+        print(f"{self.name} атакует врага!")
+
+
+class Archer(Heroes):
+    def __init__(self, name, hp, arrows, precision):
+        super().__init__(name, hp)
+        self.arrows = arrows
+        self.precision = precision
+
+    def attack(self):
+        if self.arrows > 0:
+            self.arrows -= 1
+            from random import random
+            if random() < self.precision:
+                print(f"{self.name} попадание!")
+            else:
+                print(f"{self.name} промах")
+        else:
+            print(f"у {self.name} закончились стрелы")
+
+    def recharge(self):
+        self.arrows += 5
+        print(f"у {self.name} перезарядка Теперь у него {self.arrows} стрел.")
+
+    def status(self):
+        return f"Имя: {self.name}, Здоровье: {self.hp}, Стрелы: {self.arrows}, Точность: {self.precision:.2f}"
+
+
+# Тестирование кода
+if __name__ == "__main__":
+    archer = Archer("deadpool", 50, 3, 0.5)
+
+    # Проверяем методы
+    print(archer.status())
+    archer.action()
+    archer.attack()
+    archer.attack()
+    archer.attack()
+    archer.attack()
+    archer.recharge()
+    print(archer.status())
---
+++ lesson/lesson3.py
@@ -0,0 +1,22 @@
+# # Наследование - Инкапсуляция - Полиморфизм - Абстракция
+# from abc import ABC, abstractmethod
+#
+# class OTPService(ABC):
+#
+#
+#     @abstractmethod
+#     def sms_send():
+#         pass
+#
+#
+# class NikitaOTP(OTPService):
+#
+#
+#     def sms_send(phone):
+#         phone = input("Введите тел в формате +7xxx xx xx xx xx")
+#
+#       mn
+# class Apnime(ABC):
+#
+#
+#     @abstractmethod
